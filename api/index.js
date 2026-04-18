eeda140a139943cfd3c729273f3402960a45f0ff
Muhammad Affan Siddiqui <m.affans913@gmail.com>
2026-04-18 17:29:51 +0500

    Refactor database connection handling

--- api/index.js
+++ api/index.js
@@ -11,27 +11,27 @@ app.get('/api', (req, res) => {
     res.send("this is main");
 })
 
-connectDb().then(() => { 
-    // app.listen(5000, () => {
-        console.log("Server has started at : http://localhost:5000");  
-    // })
-}).catch((err) => {
-    console.log('error');
-    console.log(err);
-})
+// connectDb().then(() => { 
+//     // app.listen(5000, () => {
+//         console.log("Server has started at : http://localhost:5000");  
+//     // })
+// }).catch((err) => {
+//     console.log('error');
+//     console.log(err);
+// })
 
-// connect DB safely (Vercel-friendly)
-// const start = async () => {
-//     try {
-//         await connectDb();
-//         console.log("DB connected");
-//     } catch (err) {
-//         console.log("DB error:", err);
-//     }
-// };
+connect DB safely (Vercel-friendly)
+const start = async () => {
+    try {
+        await connectDb();
+        console.log("DB connected");
+    } catch (err) {
+        console.log("DB error:", err);
+    }
+};
 
-// start();
+start();
 
-app.use('/api/data', Data);
+// app.use('/api/data', Data);
 
 module.exports = app;
